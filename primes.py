2619e8593c2f523bfe1917f9ef9ca2ef69373c07
Parneet Johal <parneetkjohal@gmail.com>
2022-10-01 14:38:37 +0100

    Add exception to check if input is less that or equal to zero

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+    i = 0
+    if number_of_primes <= 0:
+        raise ValueError(f'Input={number_of_primes} should have been a positive number greater than 0')
+    else:
+        while(len(list) < number_of_primes):
+            if i > 1:
+                for j in range(2, i):
+                    if (i % j) == 0:
+                        break
+                else:
+                    list.append(i)
+            i += 1
     return list
